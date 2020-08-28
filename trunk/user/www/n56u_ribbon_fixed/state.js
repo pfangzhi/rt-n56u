333b14df9ae452534c549d0acd9095364e9487c4
pfangzhi <69944031+pfangzhi@users.noreply.github.com>
2020-08-28 10:39:15 +0800

    Update state.js

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -608,19 +608,7 @@ function show_menu(L1, L2, L3){
 		$("tabMenu").innerHTML = "";
 }
 
-function show_footer(){
-	footer_code = '<div align="center" class="bottom-image"></div>\n';
-	footer_code +='<div align="center" class="copyright"><#footer_copyright_desc#></div>\n';
-	footer_code +='<div align="center">\n';
-	footer_code +='  <span>Highcharts by Torstein Hønsi & <a href="http://www.highcharts.com">Highsoft</a></span></br>\n';
-	footer_code +='  <span>Big icons designed by <a href="http://www.freepik.com">Freepik</a></br></span>\n';
-	footer_code +='  <span>Non-Commercial Use Only</span></br>\n';
-	footer_code +='</div>\n';
-
-	$("footer").innerHTML = footer_code;
-
-	flash_button();
-}
+function show_footer(){}
 
 function show_top_status(){
 	var $j = jQuery.noConflict();
